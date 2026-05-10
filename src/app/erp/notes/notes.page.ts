6fe7155fee837223d32dd0629bcdbd3c8cdcd915
Desmond8500 <desmond8500@gmail.com>
2026-05-10 19:41:18 +0000

    feat: ajouter un bouton pour créer une note, refactoriser le composant de notes et améliorer le style

--- src/app/erp/notes/notes.page.ts
+++ src/app/erp/notes/notes.page.ts
@@ -1,16 +1,23 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonButton } from '@ionic/angular/standalone';
 import { ActivatedRoute } from '@angular/router';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
+import { ProjectNotesCardComponent } from '../projects/project-notes-card/project-notes-card.component';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.page.html',
   styleUrls: ['./notes.page.scss'],
   standalone: true,
-  imports: [IonContent, CommonModule, FormsModule, HeaderComponent],
+  imports: [IonButton,
+    IonContent,
+    CommonModule,
+    FormsModule,
+    ProjectNotesCardComponent,
+    HeaderComponent,
+  ],
 })
 export class NotesPage implements OnInit {
   back: any;
