41588a7617443b5b1c26431c1429e15ed37d8c9f
Desmond8500 <desmond8500@gmail.com>
2026-05-17 18:58:29 +0000

    feat: refactor le composant Sidebar pour intégrer un menu dynamique avec sous-menus et améliorer la structure du code

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 import {
   IonButton,
   IonContent,
@@ -16,7 +17,8 @@ import {
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
-  imports: [IonButtons,
+  imports: [
+    IonButtons,
     IonFooter,
     IonButton,
     IonContent,
@@ -31,9 +33,45 @@ import {
   ],
 })
 export class SidebarComponent implements OnInit {
+
+  menus = [
+    {
+      id: 1,
+      name: 'Authentification',
+      list: [
+        { id: 1, name: 'Connexion', route: '/login' },
+        { id: 2, name: 'Dashboard', route: '/dashboard' },
+      ],
+    },
+    {
+      id: 2,
+      name: 'ERP',
+      list: [
+        { id: 1, name: 'Clients', route: '/clients' },
+        { id: 2, name: 'Devis', route: '' },
+        { id: 3, name: 'Finances', route: '' },
+        { id: 4, name: 'Journaux', route: '' },
+        { id: 5, name: 'Forfaits', route: '' },
+        { id: 6, name: 'Plannings', route: '' },
+        { id: 7, name: 'Equipe', route: '' },
+        { id: 8, name: 'Systèmes', route: '' },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Stock',
+      list: [
+        { id: 1, name: 'Articles', route: '/articles' },
+        { id: 2, name: 'Marques', route: '/brands' },
+        { id: 3, name: 'Fournisseurs', route: '/providers' },
+      ],
+    },
+  ];
+
   constructor() {
     addIcons({ close });
   }
 
   ngOnInit() {}
+
 }
